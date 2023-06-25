90589a99861aabf9d2db40c2a5c9cad1c176194a
“New <124679795+Renshot91@users.noreply.github.com>
2023-06-25 15:18:21 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+                char myChar = 'G';
+                int myInt = 89;
+                byte myByte = 4;
+                short myShort = 56;
+                float myFloat = 4.7333436f;
+                double myDouble = 4.355453532;
+                long myLong = 12121;
+
+                Character myCharacter = 'G';
+                Integer myInteger = 89;
+                Byte myByteWrapper = 4;
+                Short myShortWrapper = 56;
+                Float myFloatWrapper = 4.7333436f;
+                Double myDoubleWrapper = 4.355453532;
+                Long myLongWrapper = 12121L;
+
+                System.out.println("char: " + myChar);
+                System.out.println("int: " + myInt);
+                System.out.println("byte: " + myByte);
+                System.out.println("short: " + myShort);
+                System.out.println("float: " + myFloat);
+                System.out.println("double: " + myDouble);
+                System.out.println("long: " + myLong);
+
+                System.out.println("Character: " + myCharacter);
+                System.out.println("Integer: " + myInteger);
+                System.out.println("Byte: " + myByteWrapper);
+                System.out.println("Short: " + myShortWrapper);
+                System.out.println("Float: " + myFloatWrapper);
+                System.out.println("Double: " + myDoubleWrapper);
+                System.out.println("Long: " + myLongWrapper);
+            }
+        }
+
